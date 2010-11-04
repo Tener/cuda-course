734a22c68f583d95cbe947be5f97760cc6221707
Krzysztof Skrzetnicki <gtener@gmail.com>
2010-11-04 01:10:00 +0100

    działający program, teraz próbujemy optymalizować

--- pracownia/p4/OdlegloscEdycyjna.cu
+++ pracownia/p4/OdlegloscEdycyjna.cu
@@ -14,21 +14,64 @@
 
 using namespace std;
 
+iconv_t iconv_from_utf8;
+iconv_t iconv_to_utf8;
+
 const int MAX_L=16;  // Maksymalna dlugosc slowa (łącznie z 0 na końcu napisu)
+const int MAX_ARG=2048; // Maksymalna liczba argumentów
+const int TILE = 16;
 
-//========================================================================
-// Funkcja testowa OE_CPU() do porównywania na CPU
-//
-// Tablice tymczasowe do uzycia w liczeniu odleglosci edycyjnej na CPU
-int gt1C[MAX_L];
-int gt2C[MAX_L];
-
-inline int minimum(const int a,const int b,const int c){
+__device__ __host__ inline int minimum(const int a,const int b,const int c){
   return a<b? (c<a?c:a): (c<b?c:b);
 }
 
+
+void printResult( char * proc, double time, char * slowo, char * najblizsze, int najblizsze_ix, int odleglosc )
+{
+  char slowo_utf8[1024];
+  char najblizsze_utf8[1024];
+
+  {
+    size_t iso_len = strlen(slowo);
+    size_t utf_len = 1024;
+
+    char * utf = slowo_utf8;
+    char * iso = slowo;
+
+    iconv( iconv_to_utf8,
+           &iso, &iso_len,
+           &utf, &utf_len);
+  }
+
+  {
+    size_t iso_len = strlen(najblizsze);
+    size_t utf_len = 1024;
+
+    char * utf = najblizsze_utf8;
+    char * iso = najblizsze;
+
+    iconv( iconv_to_utf8,
+           &iso, &iso_len,
+           &utf, &utf_len);
+  }
+
+
+  printf("%s time: %2.6f (ms)\n", proc, time);
+//  printf("Wzorzec:'%16s' Znaleziony(ix=%03d:'%16s' (odleglosc=%3d)\n",
+//         slowo_utf8, najblizsze_ix, najblizsze_utf8, odleglosc);
+//  printf("Wzorzec:'%16s' Znaleziony(ix=%03d:'%16s' (odleglosc=%3d)\n",
+//         slowo, najblizsze_ix, najblizsze, odleglosc);
+}
+
+inline int maximum( int a, int b ){ return a > b ? a : b; }
+
+/////////////// CPU ///////////////
 // Odległosc edycyjna Levenshtein'a
-inline int OE_CPU(char *a,const int aN,char *b,const int bN){
+__host__ __device__ inline
+int OE_CPU(const char *a,const int aN,const char *b,const int bN){
+  int gt1C[MAX_L];
+  int gt2C[MAX_L];
+
   int *d1 = gt1C;
   int *d2 = gt2C;
 
@@ -48,23 +91,173 @@ inline int OE_CPU(char *a,const int aN,char *b,const int bN){
   return d1[bN];
 }
 
-inline int maximum( int a, int b ){ return a > b ? a : b; }
 
-//========================================================================
-// Bardzo prymitywny test funkcji OE_CPU i wzorzec jak i czym mierzyć czas
-//
+void runCPU( char * slowo, char * slownik, int rozmiar_slownika,
+             int * najblizsze_slowo, int * odleglosc)
+{
+  char *s = slowo;
+  int minlCPU=999999, miniCPU;
+  for (int i=0; i<rozmiar_slownika; i++)
+    {
+      char *d=slownik+MAX_L*i;
+      int   l = OE_CPU(s,  strlen(s),    d,  strlen(d));
+      if (l<minlCPU){   minlCPU=l;  miniCPU=i; }
+#ifdef DEBUG
+      printf("%02d: %d%s", i, l, (i+1) % 4 ? "\t" : "\n" );
+#endif
+    }
+
+#ifdef DEBUG
+  printf("\n");
+#endif
+
+  *najblizsze_slowo = miniCPU;
+  *odleglosc = minlCPU;
+
+}
+
+/////////////// GPU ///////////////
+__global__
+void kernelGPU_OE(char * slownik, int rozmiar_slownika, char * slowo, int dlugosc_slowa, char * wyniki)
+{
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if ( idx >= rozmiar_slownika )
+    {
+      wyniki[idx] = 0;
+      return;
+    }
+
+  // TODO: use shared memory here instead.
+  //  int gt1C[MAX_L];
+  //  int gt2C[MAX_L];
+
+  char a[MAX_L]; // słowo ze słownika
+  char * b = slowo;
+
+  int aN = 0; // calculated below
+  int bN = dlugosc_slowa;
+
+  // loop is good here, can be unrolled at compile time.
+  // TODO: consider #pragma unroll(MAX_L)
+#pragma unroll 16
+  for(int i=0; i < MAX_L; i++)
+    {
+      if (!slownik[MAX_L*idx+i])
+        {
+          aN = aN == 0 ? i : aN;
+          break; // TODO: check whether is't good to break here
+        }
+      a[i] = slownik[MAX_L*idx+i];
+    }
+
+  //  __syncthreads(); // coalescent memory access
+  int val = OE_CPU( a, aN, b, bN );
+  //wyniki[idx] = val;
+
+  //reverse_wyniki[val] = idx;
+  atomicMin(reverse_wyniki[val], idx);
+
+
+// 
+//  for (int j=0;j<=bN;j++) d1[j]=j;
+// 
+//  for (int i=1;i<=aN;i++) {
+//    d2[0] = i;
+//    for (int j=1;j<=bN;j++) {
+//      d2[j] = minimum(d1[j  ] + 1,                        // deletion
+//                      d2[j-1] + 1,                        // insertion
+//                      d1[j-1] + ((a[i-1]==b[j-1])? 0:1)); // substitution
+//    }
+//    d1 = (d1==gt1C)? gt2C:gt1C; // table exchange 1<>2
+//    d2 = (d2==gt2C)? gt1C:gt2C; // table exchange 1<>2
+//  }
+//  wyniki[idx] = d1[bN];
+
+}
+
+__global__
+void kernelGPU_min(char * wyniki, int rozmiar_slownika, dim3 * wynik)
+{
+  int minOdl = 9999;
+  int minIdx;
+
+  for(int i=0; i < rozmiar_slownika; i++)
+    {
+      if (wyniki[i] < minOdl)
+        {
+          minOdl = wyniki[i];
+          minIdx = i;
+        }
+    }
+
+   *wynik = dim3( minOdl, minIdx );
+}
+
+__host__
+void runGPU( char * slowo, char * slownik_gpu, int rozmiar_slownika,
+             int * najblizsze_slowo, int * odleglosc)
+{
+  int liczba_watkow = (1+(rozmiar_slownika / TILE)) * TILE;
+
+  char * slowo_gpu;
+  cudaMalloc(&slowo_gpu, MAX_L);
+  cudaMemcpy(slowo_gpu, slowo, MAX_L, cudaMemcpyHostToDevice);
+
+  char * wyniki_gpu;
+  cudaMalloc(&wyniki_gpu, liczba_watkow);
+
+  // Wywołanie jądra
+  dim3 dimGrid(liczba_watkow / TILE);
+  dim3 dimBlock(TILE);
+  kernelGPU_OE<<<dimGrid, dimBlock>>>(slownik_gpu, rozmiar_slownika, slowo_gpu, strlen(slowo), wyniki_gpu);
+  cudaThreadSynchronize();
+
+  dim3 wynik;
+  dim3 * wynik_gpu;
+  cudaMalloc(&wynik_gpu, sizeof(dim3));
+  kernelGPU_min<<< 1, 1 >>>(wyniki_gpu, rozmiar_slownika, wynik_gpu);
+  cudaMemcpy(&wynik,wynik_gpu, sizeof(dim3), cudaMemcpyDeviceToHost);
+
+  *odleglosc = wynik.x;
+  *najblizsze_slowo = wynik.y;
+
+#ifdef DEBUG
+  char * wyniki = (char *) malloc(liczba_watkow);
+  cudaMemcpy(wyniki,wyniki_gpu, liczba_watkow, cudaMemcpyDeviceToHost);
+  for(int i=0; i < rozmiar_slownika; i++)
+    {
+      printf("%02d: %d%s", i, wyniki[i], (i+1) % 4 ? "\t" : "\n" );
+    }
+  printf("\n");
+  free(wyniki);
+#endif
+
+#ifdef DEBUG
+  cudaFree(slowo_gpu);
+  cudaFree(wyniki_gpu);
+  cudaFree(wynik_gpu);
+  cudaThreadSynchronize();
+#endif
+
+}
+
+
 int main(int argc, char** argv){
-  setlocale(LC_ALL, "ISO-8859-2");
-
   if ( argc < 3 )
     {
       printf("Za mało argumentów\n");
       return 1;
     }
 
+  if ( argc >= MAX_ARG )
+    {
+      printf("Za dużo argumentów\n");
+      return 1;
+    }
+
   // --------------------------------------------------------------------
   // Zaladuj slownik
-  FILE *plik_slownika =         fopen(argv[1], "r");  // pierwszy argument !!!
+  FILE *plik_slownika = fopen(argv[1], "r");  // pierwszy argument !!!
   if (!plik_slownika)
     {
       printf("Nie udało się otworzyć pliku słownika: %s\n", argv[1]);
@@ -79,7 +272,9 @@ int main(int argc, char** argv){
       char buf[1024];
       fscanf(plik_slownika,"%s",buf);
       int dl = strlen(buf);
+#ifdef DEBUG
       printf("Długość słowa: %d\nSłowo: %s\n", dl, buf);
+#endif
       max_dl = maximum( max_dl, dl );
       ilosc++;
     }
@@ -90,6 +285,7 @@ int main(int argc, char** argv){
   rewind(plik_slownika);
   size_t slownik_size = sizeof(char) * MAX_L * ilosc;
   char * slownik = (char *) malloc(slownik_size);
+  int slownik_rozmiar;
   memset(slownik, 0, slownik_size);
 
   int cnt=0;
@@ -102,14 +298,21 @@ int main(int argc, char** argv){
         continue;
 
       memcpy(slownik + cnt * MAX_L, buf, MAX_L);
+#ifdef DEBUG
       printf("SŁOWO: %s\n", slownik + cnt * MAX_L);
+#endif
       cnt++;
     }
+  slownik_rozmiar = cnt;
 
   printf("Wczytano %d słów, %d nowych linii\n", cnt, ilosc);
 
   /* konwertujemy argumenty */
-  iconv_t iconv_from_utf8 = iconv_open("ISO-8859-2","UTF-8");
+  iconv_from_utf8 = iconv_open("ISO-8859-2","UTF-8");
+  iconv_to_utf8 = iconv_open("UTF-8","ISO-8859-2");
+
+  char * arguments[MAX_ARG];
+
   for(int i = 0; i < argc; i++)
     {
       size_t bufLen = sizeof(char) * 1024;
@@ -123,65 +326,57 @@ int main(int argc, char** argv){
              &arg_i, &argvLen,
              &buf_i, &bufLen);
 
+      arguments[i] = buf;
+
+#ifdef DEBUG
       printf("argv[%d] = %s(%d) = %s(%d)\n",
              i,
              argv[i], strlen(argv[i]),
              buf, strlen(buf));
+#endif
     }
 
-#ifdef NOTDEF
-
-  // ...   do prostego testu chwilowo wstawiamy tablice ze slowami co 16
-  int  nSlow=3;
-  char pSlowa[] = "domk\0          \ndommek\0        \ndonek\0         \n";
 
   // --------------------------------------------------------------------
   // -GPU:---------------------------------------------------------------
   // --------------------------------------------------------------------
-  // Slownik możemy przeslać na GPU tutaj: tego nie liczymy do czasu
-  // Tylko słownik, nie słowa !
-  // ...
+  /* przesyłanie słownika na GPU */
 
-  // Testowanie na wszystkich slowach ktore sa kolejnymu argumentami
-  // wywolania programu (argv, poza pierwszym: plik slownika) GPU/CPU
-  // --------------------------------------------------------------------
-  unsigned int timer = 0;
-  cutilCheckError( cutCreateTimer( &timer));
-  cutilCheckError( cutStartTimer(   timer));
+  char * slownik_GPU;
+  cudaMalloc(&slownik_GPU, slownik_size);
+  cudaMemcpy(slownik_GPU, slownik, slownik_size, cudaMemcpyHostToDevice);
 
-  // GPU load run get
-  // ...
-
-  printf( "GPU time: %2.6f (ms)\n", cutGetTimerValue( timer));
-  cutilCheckError( cutDeleteTimer( timer));
-  // GPU rezultaty wypisać tu:
-  // ...
-
-  // --------------------------------------------------------------------
-  // -CPU:---------------------------------------------------------------
-  // --------------------------------------------------------------------
-  cutilCheckError( cutCreateTimer( &timer));
-  cutilCheckError( cutStartTimer( timer));
-
-  // CPU run
-  // ...
-  char *s=argv[2];  // na poczatek tylko pierwsze slowo <- testowac wszystkie
-  int minlCPU=999999, miniCPU;
-  for (int i=0; i<nSlow; i++)
+  for(int argNum = 2; argNum < argc; argNum++)
     {
-      char *d=pSlowa+MAX_L*i;
-      int   l = OE_CPU(s,  strlen(s),    d,  strlen(d));
-      if (l<minlCPU){   minlCPU=l;  miniCPU=i; }
+      char * slowo = arguments[argNum];
+
+      {
+        unsigned int timer = 0;
+        int numer_slowa = 0, odleglosc = 999;
+        // CPU
+        cutilCheckError( cutCreateTimer( &timer));
+        cutilCheckError( cutStartTimer( timer));
+        runCPU( slowo, slownik, slownik_rozmiar, &numer_slowa, &odleglosc );
+        printResult("CPU", cutGetTimerValue( timer), slowo, slownik+MAX_L*numer_slowa, numer_slowa, odleglosc);
+        cutilCheckError( cutDeleteTimer( timer));
+      }
+
+      {
+        unsigned int timer = 0;
+        int numer_slowa = 0, odleglosc = 999;
+        // GPU
+        cutilCheckError( cutCreateTimer( &timer));
+        cutilCheckError( cutStartTimer( timer));
+        runGPU( slowo, slownik_GPU, slownik_rozmiar, &numer_slowa, &odleglosc );
+        printResult("GPU", cutGetTimerValue( timer), slowo, slownik+MAX_L*numer_slowa, numer_slowa, odleglosc);
+        cutilCheckError( cutDeleteTimer( timer));
+      }
+
     }
-  printf( "CPU time: %2.6f (ms)\n", cutGetTimerValue( timer));
-  cutilCheckError( cutDeleteTimer( timer));
 
-  // CPU rezultaty wypisać tu:
-  printf("Wzorzec:%16s Znaleziony:%16s (odleglosc=%3d)\n",
-         s,      pSlowa+MAX_L*miniCPU, minlCPU);
-
-  // --------------------------------------------------------------------
-#endif
+  cudaFree(slownik_GPU);
+  iconv_close(iconv_from_utf8);
+  iconv_close(iconv_to_utf8);
 
   return 0;
 
