9148da61ddc70640fcfc87825ec783aa0c90774b
Krzysztof Skrzetnicki <gtener@gmail.com>
2010-11-07 20:21:09 +0100

    zrandomizowane testy

--- pracownia/p4/tester.hs
+++ pracownia/p4/tester.hs
@@ -3,9 +3,14 @@ module Main where
 import System.Environment
 import System.Directory
 import System.FilePath
+import System.Random
 import System.Process
 import System.IO
+
 import Data.List.Split
+
+import Control.Applicative
+import Control.Monad
 
 import Text.Printf
 import Text.EditDistance
@@ -45,26 +50,47 @@ runTest nazwaProgramu slownik slowa = do
   let (p1,p2_unparsed) = oneProc (lines cont)
       (p2,nothing) = oneProc p2_unparsed
 
-  print nothing
+  case nothing of
+    [] -> return ()
+    _ -> error "Nie sparsowane do końca wejście"
 
 --  return (p1,p2)
 
-  let formatBatch (proc, res) = unlines (map (\(w1,w2,dist) -> printf "%s ==> %s -> %s : %d %s" proc w1 w2 dist (show $ validate w1 w2 dist)) res)
-  putStrLn $ formatBatch p1
-  putStrLn $ formatBatch p2
+  let formatBatch (proc, res) = (map (\(w1,w2,dist) -> printf "%s ==> %s -> %s : %d %s" proc w1 w2 dist (show $ validate w1 w2 dist)) res)
+      formatOneResult proc (w1,w2,dist) (dist2,match) = printf "%s ==> %s -> %s : %d(%d) %s" proc w1 w2 dist dist2 (show match)
+
+  let checkResults (proc,res) = do
+        let errors = [ (w1,formatOneResult proc r v) | r@(w1,w2,dist) <- res, v@(dist2,match) <- [validate w1 w2 dist], not match]
+        unless (null errors) (do
+                               putStrLn "Error occured:"
+                               mapM_ putStrLn (map snd errors)
+                               putStrLn "Try it yourself:"
+                               putStrLn (unwords ([nazwaProgramu,slownik]++ map fst errors))
+                               error "fail"
+                             )
+  mapM_ checkResults [p1,p2]
 
 
 
 validate input output distance = let dist' = levenshteinDistance (EditCosts 1 1 1 1) input output in (dist', dist' == distance)
 
 main = do
-  -- test ze słownika
   [slownik, nazwaProgramu] <- getArgs
 
-  print =<< runTest slownik nazwaProgramu ["testowość", "przewlokły", "woretewr", "żywotnikowiek", "gżegżułka"]
+  -- test ze słownika
 
   -- test losowy
+  letters <- randomRs ('a','z') <$> newStdGen
+  wordLenghts <- randomRs (3,10) <$> newStdGen
+  let batchCount = 100
+      batchSize = 10
+      batches = take batchCount $ splitEvery batchSize (cut letters wordLenghts)
+
+      cut xs (z:zs) = let (a,b) = splitAt z xs in a : cut b zs
+
+  mapM_ (runTest slownik nazwaProgramu) batches
 
   -- test wybranych kombinacji słów
+  runTest slownik nazwaProgramu ["testowość", "przewlokły", "woretewr", "żywotnikowiek", "gżegżułka"]
 
   return ()
