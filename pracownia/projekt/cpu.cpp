8cdf6a7e6ea53ad34cae5dbd3ae18d3cfc216b54
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-01-12 04:27:16 +0100

    Fix how points are generated in CPU

--- pracownia/projekt/cpu.cpp
+++ pracownia/projekt/cpu.cpp
@@ -9,39 +9,18 @@ namespace hull {
 namespace alg {
 namespace cpu {
 
-  // http://mathworld.wolfram.com/CirclePointPicking.html
-  Point random_point(){ 
-    double x, y;
-    do {
-      x = random_coord();
-      y = random_coord();
-    }
-    while ( x*x + y*y >= 1 );
-    
-    return {x,y};
-  }
-    
-#if 0
   Point random_point(){ 
     // http://www.anderswallin.net/2009/05/uniform-random-points-in-a-circle-using-polar-coordinates/
     // http://www.comnets.uni-bremen.de/itg/itgfg521/per_eval/circle_uniform_distribution.pdf
     double r = sqrt( random_radius() );
-    double theta = random_angle();
+    double theta = random_radius() * 2 * M_PI;
 
     // http://en.wikipedia.org/wiki/Polar_coordinate_system
     double x = r * cos(theta);
     double y = r * sin(theta);
-    
-    cout << 
-      "X:" << x << " " <<
-      "Y:" << y << " " <<
-      "R:" << r << " " << 
-      "T:" << theta << " " <<
-      endl;
 
     return { x, y };
   }
-#endif
 
   void draw_point( const Point & p, GLfloat size = 1.0 )
   {
