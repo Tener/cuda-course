2413a381b5f5ae3dbad4544bae6aff3c69efb337
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-01-19 16:42:59 +0100

    Glory mess. It works, but convex hull is actually calculated on CPU.

--- pracownia/projekt/kernel.cu
+++ pracownia/projekt/kernel.cu
@@ -20,12 +20,14 @@
 #include <thrust/gather.h>
 #include <thrust/generate.h>
 #include <thrust/host_vector.h>
+#include <thrust/partition.h>
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/random.h>
 #include <thrust/remove.h>
 #include <thrust/sequence.h>
 #include <thrust/sort.h>
 
+#include "cpu.hpp"
 
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
       printf("Error (%d) at %s:%d\n", __FILE__,__LINE__);	\
@@ -208,23 +210,110 @@ struct CompareY : public thrust::binary_function<Float2,Float2,bool>
 
 struct RemoveQuadrilateral
 {
-  float p0_x, p1_x, p2_x, p3_x, p0_y, p1_y, p2_y, p3_y;
+  float x0, x1, x2, x3, y0, y1, y2, y3;
 
-  RemoveQuadrilateral(float p0_x,float p0_y, //p0 - min x
-		      float p1_x,float p1_y, //p1 - max x
-		      float p2_x,float p2_y, //p2 - min y
-		      float p3_x,float p3_y) //p3 - max y
-    : p0_x(p0_x), p1_x(p1_x), p2_x(p2_x), p3_x(p3_x), 
-      p0_y(p0_y), p1_y(p1_y), p2_y(p2_y), p3_y(p3_y) { }
+  // http://stackoverflow.com/questions/243945/calculating-a-2d-vectors-cross-product
+  __host__ __device__ inline
+  float crossProduct( float x0, float y0, float x1, float y1 )
+  {
+    return x0*y1 - y0*x1;
+  }
 
-  __host__ __device__ bool operator()( thrust::tuple< float, float > el )
+  RemoveQuadrilateral(float x0,float y0, //min x
+		      float x1,float y1, //max x
+		      float x2,float y2, //min y
+		      float x3,float y3) //max y
+    : x0(x0), x1(x1), x2(x2), x3(x3), 
+      y0(y0), y1(y1), y2(y2), y3(y3) { }
+
+  __host__ __device__ 
+  bool operator()( thrust::tuple< float, float > el )
   {
     float x = thrust::get<0>(el) * cos(thrust::get<1>(el));
     float y = thrust::get<0>(el) * sin(thrust::get<1>(el));
 
-    return x < 0;
+    // (A − B) × (p − B) > 0
+    // http://stackoverflow.com/questions/243945/calculating-a-2d-vectors-cross-product
+    // { x0-x1, y0-y1 } × { x-x1, y-y1 };
+    bool above_0_1 = (x0-x1)*(y-y1) - (y0-y1)*(x-x1) < 0;
+    bool above_1_2 = (x1-x2)*(y-y2) - (y1-y2)*(x-x2) < 0;
+    bool above_2_3 = (x2-x3)*(y-y3) - (y2-y3)*(x-x3) < 0;
+    bool above_3_0 = (x3-x0)*(y-y0) - (y3-y0)*(x-x0) < 0;
+
+    return !(above_0_1 || above_1_2 || above_2_3 || above_3_0);
+}
+
+};
+
+
+struct Visualize
+{
+  float4 * vbo;
+  Visualize(float4 * vbo) : vbo(vbo) { }
+  template <typename Tuple>
+  __host__ __device__
+  void operator()(Tuple t)
+  {
+    vbo[thrust::get<0>(t)] = make_float4( thrust::get<1>(t), thrust::get<2>(t), 0.0f, 1.0f );
+  }
+};
+
+struct Visualize2
+{
+  thrust::device_ptr< float4 > vbo;
+  Visualize2(float4 * vbo) : vbo(vbo) { }
+  template <typename Tuple>
+  __host__ __device__
+  void operator()(Tuple t)
+  {
+    vbo[thrust::get<0>(t)] = make_float4( thrust::get<0>(thrust::get<1>(t)), 
+					  thrust::get<1>(thrust::get<1>(t)),
+					  0.0f, 1.0f);
+  }
+};
+
+
+struct PolarIntoPlanar
+{
+  template <typename Tuple>
+  __host__ __device__
+  Tuple operator()(Tuple t)
+  {
+    float r = thrust::get<0>(t);
+    float th = thrust::get<1>(t);
+    return thrust::make_tuple( r * cos(th), r * sin(th) );
+  }
+};
+
+struct EarlyPartition
+{
+  float x0, y0, x1, y1;
+  EarlyPartition(float x0, float y0, float x1, float y1) : x0(x0), y0(y0), x1(x1), y1(y1) 
+  {
   }
 
+  template <typename Tuple>
+  __host__ __device__
+  bool operator()(Tuple t)
+  {
+    float x = thrust::get<0>(t);
+    float y = thrust::get<1>(t);
+    return (x0-x1)*(y-y1) - (y0-y1)*(x-x1) >= 0;
+  }
+};
+
+struct mkPoint : public thrust::unary_function< thrust::tuple< float, float > , Point >
+{
+  template <typename Tuple>
+  __host__ __device__
+  Point operator()(Tuple t)
+  {
+    //int ix = thrust::get<0>(t);
+    float x = thrust::get<0>(t);
+    float y = thrust::get<1>(t);
+    //vec[ix] = Point(x,y);
+    return Point(x,y);
+  }
 };
 
 
@@ -233,7 +322,20 @@ extern "C" void launch_kernel_random_points(float4* vbo1, int* vbo1_vert_cnt,
 					    float4* vbo2, int* vbo2_vert_cnt,
 					    unsigned int points)
 {
+  //  sleep(1);
+
+  cudaEvent_t start;
+  cudaEvent_t end;
+  float elapsed_time;
+    
+  cudaEventCreate(&start);
+  cudaEventCreate(&end);
+
+  cudaEventRecord(start,0);
+
+
   //  std::cout << "cores: " << getCoreCount() << std::endl;
+  //unsigned int points_orig = points;
 
   points *= 1024;
   *vbo1_vert_cnt = points;
@@ -244,14 +346,21 @@ extern "C" void launch_kernel_random_points(float4* vbo1, int* vbo1_vert_cnt,
   thrust::transform( thrust::counting_iterator< int >(0),
 		     thrust::counting_iterator< int >(points),
 		     thrust::make_zip_iterator(make_tuple( oP_r.begin(), oP_theta.begin())),
-		     //		     thrust::make_zip_iterator(make_tuple( oP_x.begin(), oP_y.begin())),
-		     KernelMakePointsDisplay(rand(),vbo1) );
+		     KernelMakePointsDisplay(rand(),vbo1) );  
 
   {
-    // find extreme points and remove the vast amount of points that lie within the bounding quadrilateral
-    float angles[] = { 0, M_PI/2, M_PI/4, -M_PI/4 };    
+    // first points on convex hull - taken from quadrilaterals
+    thrust::host_vector<float> h_x;
+    thrust::host_vector<float> h_y;
+    thrust::host_vector<float> h_t;
+    thrust::host_vector<float> h_r;
+    thrust::host_vector< thrust::tuple< float, float > > h_rt;
 
-    for(int i = 0; i < 1; i++)
+    // find extreme points and remove the vast amount of points that lie within the bounding quadrilateral
+    float angles[] = { 0 * (M_PI/8), 1 * (M_PI/8), 2 * (M_PI/8), 3 * (M_PI/8) };
+
+
+    for(int i = 0; i < 4; i++)
       {
 	DevVecFloatZipIterTuple oP_begin = thrust::make_zip_iterator(make_tuple( oP_r.begin(), oP_theta.begin()));
 	DevVecFloatZipIterTuple oP_end = thrust::make_zip_iterator(make_tuple( oP_r.end(), oP_theta.end()));
@@ -266,51 +375,168 @@ extern "C" void launch_kernel_random_points(float4* vbo1, int* vbo1_vert_cnt,
 	minmax_y = thrust::minmax_element( oP_begin,
 					   oP_end,
 					   CompareY(alpha) );
-					   
-	DevVecFloatZipIterTuple new_oP_end = thrust::remove_if( oP_begin,
-								oP_end, 
-								RemoveQuadrilateral( 0, 0, 0, 0, 1, 1, 1, 1 ));
+				
+	float r0 = thrust::get<0>(*minmax_x.first),  t0 = thrust::get<1>(*minmax_x.first);
+	float r1 = thrust::get<0>(*minmax_x.second), t1 = thrust::get<1>(*minmax_x.second);
+	float r2 = thrust::get<0>(*minmax_y.first),  t2 = thrust::get<1>(*minmax_y.first);
+	float r3 = thrust::get<0>(*minmax_y.second), t3 = thrust::get<1>(*minmax_y.second);
 
+	float x0 = r0 * cos(t0), y0 = r0 * sin(t0);
+	float x1 = r1 * cos(t1), y1 = r1 * sin(t1);
+	float x2 = r2 * cos(t2), y2 = r2 * sin(t2);
+	float x3 = r3 * cos(t3), y3 = r3 * sin(t3);
+	
+	h_rt.push_back( thrust::make_tuple( r0, t0 ) ); h_t.push_back( t0 );
+	h_rt.push_back( thrust::make_tuple( r1, t1 ) ); h_t.push_back( t1 ); 
+	h_rt.push_back( thrust::make_tuple( r2, t2 ) ); h_t.push_back( t2 ); 
+	h_rt.push_back( thrust::make_tuple( r3, t3 ) ); h_t.push_back( t3 ); 
 
+	if ( 0 )
 	{
  	  using namespace thrust;
-	  std::cerr << "DIFF:" << oP_end - new_oP_end << std::endl;
-	  
+
 	  std::cerr << 
-	    "MINmax_x: " << get<0>(*minmax_x.first) << " " << get<1>(*minmax_x.first) << std::endl << 
+	    "MINmax_x: " << get<0>(*minmax_x.first)  << " " << get<1>(*minmax_x.first)  << std::endl << 
 	    "minMAX_x: " << get<0>(*minmax_x.second) << " " << get<1>(*minmax_x.second) << std::endl;
  
 	  std::cerr << 
-	    "MINmax_y: " << get<0>(*minmax_y.first) << " " << get<1>(*minmax_y.first) << std::endl << 
+	    "MINmax_y: " << get<0>(*minmax_y.first)  << " " << get<1>(*minmax_y.first)  << std::endl << 
 	    "minmax_y: " << get<0>(*minmax_y.second) << " " << get<1>(*minmax_y.second) << std::endl;
 	}
 
+
+	DevVecFloatZipIterTuple new_oP_end = thrust::remove_if( oP_begin,
+								oP_end, 
+								RemoveQuadrilateral( x0, y0, 
+										     x2, y2, 
+										     x1, y1, 
+										     x3, y3 ));
+
+	//std::cerr << "DIFF:" << oP_end - new_oP_end << std::endl;
 	oP_end = new_oP_end;
 
 	// update number of points
-	std::cerr << "P:" << points << std::endl;
+	//std::cerr << "P:" << points << std::endl;
 	points = oP_end - oP_begin;
-	std::cerr << "P:" << points << std::endl;
+	//std::cerr << "P:" << points << std::endl;
 
 	oP_r.resize(points);
 	oP_theta.resize(points);	
       }
 
+    // materialize points (x,y) now
+    thrust::device_vector<float> oP_x(points);
+    thrust::device_vector<float> oP_y(points);
+
+    thrust::transform( thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(0),
+							     oP_r.begin(), 
+							     oP_theta.begin())), 
+		       thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(points),
+							     oP_r.end(), 
+							     oP_theta.end())), 
+		       thrust::make_zip_iterator(make_tuple( oP_x.begin(), oP_y.begin())),
+		       MakePoints(vbo2) );  
+    
+    //
+    {
+      h_x.reserve(h_rt.size());
+      h_y.reserve(h_rt.size());
+      
+      thrust::sort_by_key( h_t.begin(), h_t.end(), h_rt.begin() );
+      thrust::transform( h_rt.begin(), 
+			 h_rt.end(), 
+			 thrust::make_zip_iterator(thrust::make_tuple(h_x.begin(), h_y.begin())),
+			 PolarIntoPlanar()
+			 );
+    }
+
+    DevVecFloatZipIterTuple begin = thrust::make_zip_iterator(make_tuple( oP_x.begin(), oP_y.begin()));
+    DevVecFloatZipIterTuple begin_0 = thrust::make_zip_iterator(make_tuple( oP_x.begin(), oP_y.begin()));
+    DevVecFloatZipIterTuple end = thrust::make_zip_iterator(make_tuple( oP_x.end(), oP_y.end()));
+    thrust::host_vector< DevVecFloatZipIterTuple > ends;
+
+    for(int i = 0; i < 16; i++)
+      {
+	float x0, y0, x1, y1;
+
+	x0 = h_x[i];
+	y0 = h_y[i];
+
+	x1 = h_x[(i+1) % 16];
+	y1 = h_y[(i+1) % 16];
+
+	//float r0, t0, r1, t1;
+// 	r0 = thrust::get<0>(h_rt[i]);
+// 	t0 = thrust::get<1>(h_rt[i]);
+// 	r1 = thrust::get<0>(h_rt[(i+1) % 16]);
+// 	t1 = thrust::get<1>(h_rt[(i+1) % 16]);
+
+ //	//	if ( i <= (points_orig % 16) )
+ //	  {
+ //	    std::cout << "i: " << i << std::endl;
+ //
+ //	    std::cout << "x0: " << x0 << " y0: " << y0 << " r0: " << r0 << " t0: " << t0 << std::endl;
+ //	    std::cout << "x1: " << x1 << " y1: " << y1 << " r1: " << r1 << " t1: " << t1 << std::endl;
+ //	    
+ //	    glColor3f( 0.1, 0.3, 0.3 );
+ //	    glPointSize(10);
+ //	    glLineWidth(3);
+ //	    glBegin(GL_LINES);
+ //	    glVertex2f( x0, y0 );
+ //	    glVertex2f( x1, y1 );
+ //	    glEnd();
+ //	    
+ //	  }
+	begin = thrust::partition( begin, end, EarlyPartition(x0, y0, x1, y1) );
+	ends.push_back(begin);
+      }
+    
+    points = begin - begin_0;
+    oP_x.resize(points);
+    oP_y.resize(points);
     *vbo2_vert_cnt = points;
+
+    thrust::for_each( thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(0),
+							    oP_x.begin(),
+							    oP_y.begin())),
+		      thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(points),
+							    oP_x.end(),
+							    oP_y.end())),
+		      Visualize(vbo2) );
+
+    thrust::host_vector< thrust::tuple< float, float > > vpoints;
+    std::vector< Point > std_vpoints;
+    std_vpoints.resize(points);
+    vpoints.resize(points);
+    
+    thrust::copy( thrust::make_zip_iterator(thrust::make_tuple(oP_x.begin(),
+							       oP_y.begin())),
+		  thrust::make_zip_iterator(thrust::make_tuple(oP_x.end(),
+							       oP_y.end())),
+		  vpoints.begin()
+		  );
+
+    std::cout << "Number of points after cut: " << points << " " << oP_x.size() << " " << oP_y.size() << " " << vpoints.size() << std::endl;
+    
+    thrust::host_vector< thrust::tuple< float, float > > chull = hull::alg::cpu::convex_hull(vpoints);
+    
+    thrust::for_each( thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(0),
+							    chull.begin())),
+		      thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(chull.size()),
+							    chull.end())),
+		      Visualize2(vbo2) );
+    
+    *vbo2_vert_cnt = chull.size();
+    std::cout << "Convex hull size: " << chull.size() << std::endl;
   }
 
-  // materialize points (x,y) now
-  thrust::device_vector<float> oP_x(points);
-  thrust::device_vector<float> oP_y(points);
+  cudaThreadSynchronize();
+  cudaEventRecord(end,0);
+  cudaEventSynchronize(end);
+  cudaEventElapsedTime(&elapsed_time, start, end);
+  std::cout << "Total time: " << elapsed_time << " milliseconds" << std::endl;
 
-  thrust::transform( thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(0),
-							   oP_r.begin(), 
-							   oP_theta.begin())), 
-		     thrust::make_zip_iterator(make_tuple( thrust::counting_iterator< int >(points),
-							   oP_r.end(), 
-							   oP_theta.end())), 
- 		     thrust::make_zip_iterator(make_tuple( oP_x.begin(), oP_y.begin())),
- 		     MakePoints(vbo2) );  
+
 #if 0
   // sort points
 
