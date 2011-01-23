222dabb8ffbf309a71ea04abf5897c2c4732efc9
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-01-23 17:51:45 +0100

    Implement wrapping classes for VBO, PBO and GLManager.

--- pracownia/projekt-cz2/graphics.cpp
+++ pracownia/projekt-cz2/graphics.cpp
@@ -2,52 +2,147 @@
 #include "common.hpp"
 #include "graphics.hpp"
 
-namespace hull {
-  namespace graphics {
+#include <cutil_gl_error.h>
 
-    int height = 600;
-    int width = 600;
+namespace rt {
+  namespace graphics {
+    VBO::VBO(unsigned int n_points, unsigned int vbo_res_flags)
+      : d_vbo_buffer(NULL), n_points(n_points)
+    {
+      //GLuint* vbo, struct cudaGraphicsResource **vbo_res, 
+	  
+      // create buffer object
+      glGenBuffers(1, &vbo);
+      glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	
+      // initialize buffer object
+      unsigned int size = n_points * 4 * sizeof(float);
+      glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
+	
+      glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+      // register this buffer object with CUDA
+      cutilSafeCall(cudaGraphicsGLRegisterBuffer(&cuda_vbo_resource, vbo, vbo_res_flags));
+  
+      CUT_CHECK_ERROR_GL();
+    }
+
+    VBO::~VBO()
+    {
+      // unregister this buffer object with CUDA
+      cutilSafeCall(cudaGraphicsUnregisterResource(cuda_vbo_resource));
+	
+      glBindBuffer(1, vbo); // is this really needed???
+      glDeleteBuffers(1, &vbo);
+	  
+      vbo = 0;
+    }
+
+    void VBO::render( int point_cnt, float3 color )
+    {
+      if ( point_cnt == -1 )
+        point_cnt = n_points;
+        
+
+      glBindBuffer(GL_ARRAY_BUFFER, vbo);
+      glVertexPointer(4, GL_FLOAT, 0, 0);
+
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glColor3f(color.x, color.y, color.z);
+      glDrawArrays(GL_POINTS, 0, point_cnt);
+      glDisableClientState(GL_VERTEX_ARRAY);
+
+    }
+
+    float4 * VBO::mapResourcesGetMappedPointer()
+    {
+      float4 *dptr;
+      size_t num_bytes; 
+
+      cutilSafeCall(cudaGraphicsMapResources(1, &cuda_vbo_resource));
+      cutilSafeCall(cudaGraphicsResourceGetMappedPointer((void **)&dptr, &num_bytes,  
+                                                         cuda_vbo_resource));
+
+      return dptr;
+    }
+
+    void VBO::unmapResources()
+    {
+      cutilSafeCall(cudaGraphicsUnmapResources(1, &cuda_vbo_resource, 0));
+    }
+  
+
+    PBO::PBO(int p_width, int p_height) : width(p_width), height(p_height), pbo(0)
+    {
+      glGenBuffersARB(1, &pbo);
+      glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, pbo);
+      glBufferDataARB(GL_PIXEL_UNPACK_BUFFER_ARB, width*height*sizeof(GLubyte)*4, 0, GL_STREAM_DRAW_ARB);
+      cutilSafeCall(cudaGLRegisterBufferObject(pbo));
+    }
+
+    void PBO::mapBufferObject()
+    {
+        cudaGLMapBufferObject((void**)&dev_pbo, pbo);
+    }
     
-    void initGlWindow(int argc, char ** argv){ 
-#ifdef USE_OPENGL
+    void PBO::unmapBufferObject()
+    {
+      cudaGLUnmapBufferObject(pbo);
+    }
+
+    void PBO::render()
+    {
+      glClear(GL_COLOR_BUFFER_BIT);
+      glDisable(GL_DEPTH_TEST);
+      glRasterPos2i(0, 0);
+      glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+    }
+
+    GLManager global_glm;
+
+    void GLManager::initGlWindow(){ 
       cudaGLSetGLDevice( cutGetMaxGflopsDeviceId() );
 
       if( !glfwInit() )
 	{
-	  cerr << "Failed to initalize GLFW" << endl;
+	  std::cerr << "Failed to initalize GLFW" << std::endl;
 	  exit( 1 );
 	}
+
+      glfwOpenWindowHint( GLFW_FSAA_SAMPLES, 4 );
+      glfwOpenWindowHint( GLFW_WINDOW_NO_RESIZE, GL_TRUE );
 
       if ( !glfwOpenWindow( width, height, 
 			    8, 8, 8, 8, 8, 8,
 			    GLFW_WINDOW ) )
 	{
-	  cerr << "Failed to open window" << endl;
+	  std::cerr << "Failed to open window" << std::endl;
 	  exit( 1 );
 	}
 
       glewInit();
       if (! glewIsSupported("GL_VERSION_2_0 ")) 
 	{
-	  cerr << "ERROR: Support for necessary OpenGL extensions missing." << endl;
+	  std::cerr << "ERROR: Support for necessary OpenGL extensions missing." << std::endl;
 	  exit( 1 );
 	}
     
-      //glEnable(GL_POINT_SMOOTH);
+
 
       glViewport(0, 0, height, width);
       glLoadIdentity();
-      glOrtho(-1.0, 1.0, -1.0, 1.0, 0.0, 1.0);
-#endif
+      //      glOrtho(-1.0, 1.0, -1.0, 1.0, 0.0, 1.0);
+      glOrtho(0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
+
     };
   
-    void closeGlWindow(){
-#ifdef USE_OPENGL   
+    void GLManager::closeGlWindow(){
       glfwCloseWindow();
       glfwTerminate();
-#endif
     };
 
+    void GLManager::reshape(int w, int h){ }
+
   } // namespace graphics
-} // namespace hull
+} // namespace rt
 
--- pracownia/projekt-cz2/graphics.hpp
+++ pracownia/projekt-cz2/graphics.hpp
@@ -1,13 +1,87 @@
 
-// includes
+#include <cuda_runtime.h>
+#include <cutil_inline.h>
+#include <cutil_gl_inline.h>
+#include <cuda_gl_interop.h>
+#include <vector_types.h>
 
-namespace hull {
-namespace graphics {
-  void initGlWindow(int argn, char ** argv);
-  void closeGlWindow();
-  void display();
-  void reshape(int x, int y);
-  void keyboard(unsigned char k, int , int );
-} // namespace graphics
-} // namespace hull
+#include <algorithm>
+
+// CUDA
+#include <cuda_runtime_api.h>
+#include <cuda.h>
+#include <curand_kernel.h>
+
+
+namespace rt {
+  namespace graphics {
+
+    class GLManager
+    {
+    public:
+      int height;
+      int width;
+
+      void initGlWindow();
+      void closeGlWindow();
+      void reshape(int w, int h);
+
+      GLManager( int w = 512, int h = 512 ) : width(w), height(h) { };
+        ~GLManager() { };
+    };
+
+    extern GLManager global_glm;
+
+    struct VBO { 
+      // vbo variables
+      GLuint vbo;
+      struct cudaGraphicsResource *cuda_vbo_resource;
+      void * d_vbo_buffer;
+      unsigned int n_points;
+
+      VBO(unsigned int n_points, unsigned int vbo_res_flags = cudaGraphicsMapFlagsWriteDiscard);
+
+      ~VBO();
+
+      void render( int point_cnt = -1, float3 color = make_float3( 0.3, 0.65, 0.23 ) );
+      float4 * mapResourcesGetMappedPointer();
+      void unmapResources();
+
+    };
+
+    struct PBO {
+      GLuint pbo;
+      uint *dev_pbo; // device ptr to PBO
+
+      int width;
+      int height;
+
+      PBO(int width, int height);
+      ~PBO() { };
+      void mapBufferObject();
+      void unmapBufferObject();
+      void render();
+    };
+
+    // much like scoped ptr
+    class PBO_map_unmap {
+    
+    private:
+      PBO & pbo;
+    
+    public:
+      PBO_map_unmap( PBO & pbo ) : pbo(pbo) {
+        pbo.mapBufferObject();
+      }
+
+      ~PBO_map_unmap() {
+        pbo.unmapBufferObject();
+      }
+
+    };
+
+
+
+  } // namespace graphics
+} // namespace rt
 
