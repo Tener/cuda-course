d62f9d53127f5eb91c1573bf1f0940372df3fef5
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-01-30 22:41:44 +0100

    To make python code implementation simpler first enum Surf has value 0.

--- pracownia/projekt-cz2/surf.h
+++ pracownia/projekt-cz2/surf.h
@@ -1,7 +1,7 @@
 #ifndef __SURF_H
 #define __SURF_H
 
-enum Surf { SURF_CHMUTOV_0 = 1, 
+enum Surf { SURF_CHMUTOV_0 = 0, 
             SURF_CHMUTOV_1,
             SURF_CHMUTOV_2,
             SURF_CHMUTOV_3,
