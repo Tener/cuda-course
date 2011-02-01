60221f74901701e855b910171c1181e4cdff2bcf
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-02-01 02:56:42 +0100

    Fix warning.

--- pracownia/projekt-cz2/utils.cpp
+++ pracownia/projekt-cz2/utils.cpp
@@ -25,7 +25,7 @@ const std::string SurfString( Surf s )
   return std::string("???");
 }
 
-void PrintVector( const float3 & Vec, char * name, std::ostream & out )
+void PrintVector( const float3 & Vec, const char * name, std::ostream & out )
 {
   char buf[4097];
   sprintf(buf, "%s=(%f,%f,%f)\n", name, Vec.x, Vec.y, Vec.z );
@@ -37,7 +37,7 @@ void PrintView( View v, std::ostream & out)
   out << "steps=" << v.steps << std::endl;
   out << "bisect_count=" << v.bisect_count << std::endl;
   out << "surf=" << v.surf << "=" << SurfString(v.surf) << std::endl;
-  PrintVector( v.StartingPoint, "start", out ); // what point is the center of our view?
-  PrintVector( v.DirectionVector, "dirVec", out ); // in which direction and how far does it reach?
+  PrintVector( v.StartingPoint, std::string("start").c_str(), out ); // what point is the center of our view?
+  PrintVector( v.DirectionVector, std::string("dirVec").c_str(), out ); // in which direction and how far does it reach?
 
 }
